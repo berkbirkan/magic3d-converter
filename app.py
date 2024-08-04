987b2191d70a45e88be9c34f977df2696b324505
Berk Birkan <berk-birkan@hotmail.com>
2024-08-04 19:02:20 +0300

    Add file.txt

--- app.py
+++ app.py
@@ -51,12 +51,9 @@ def convert_file():
             "https": proxy_entry
     }
 
-    headers = {
-            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36"
-    }
 
     try:
-        response = requests.get(url, headers=headers,proxies=proxies)
+        response = requests.get(url, proxies=proxies)
         response.raise_for_status()
         with open(temp_file_path, 'wb') as f:
             f.write(response.content)
