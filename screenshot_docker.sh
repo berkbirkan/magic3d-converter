b8e8b8ad95edd9278cb2d0cf64a637150b16b9a9
Berk Birkan <berk-birkan@hotmail.com>
2024-08-06 13:57:13 +0300

    Add file.txt

---
+++ screenshot_docker.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+set -eu
+
+if [[ ${#} -lt 1 ]]; then
+ echo "Usage: ${0} <gltf-file-path> [<additional-arguments>]"
+ echo "Example: ${0} ../gltf/Box.glb --count 3 --headless"
+ echo "You can pull a docker image instead of building locally by setting DOCKER_IMAGE."
+ echo "Example: DOCKER_IMAGE=bwasty/gltf-viewer:latest ${0} ../gltf/Box.glb"
+ exit 1
+fi
+
+gltf_path=$1
+gltf_dir=$(dirname "$gltf_path")
+gltf_file=$(basename "$gltf_path")
+model_name=${gltf_file%.*}
+output_file="$gltf_dir/$model_name.png"
+
+if [[ ! -z "${DOCKER_IMAGE:-}" ]]; then
+ docker pull "$DOCKER_IMAGE"
+ image="$DOCKER_IMAGE"
+else
+ docker build -t gltf-viewer .
+ image=gltf-viewer
+fi
+
+rm "$output_file" || true
+
+docker run -t --rm -v "$(pwd)/$gltf_dir:/input" \
+ $image "/input/$gltf_file" -s "/input/$model_name.png" "${@:2}"
